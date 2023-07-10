93e0dfd4f50cfc429163065effd848967cfee89d
minahil27 <minahilferoz27@gmail.com>
2023-07-10 22:52:46 +0500

    designed first screen

---
+++ app/src/main/java/com/example/hifzquran/Student_Details.java
@@ -0,0 +1,14 @@
+package com.example.hifzquran;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Student_Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_details);
+    }
+}
